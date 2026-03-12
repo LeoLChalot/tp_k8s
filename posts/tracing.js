9a9ffeac927f042bbd676492f924e1e7a4a69f5a
LeoLChalot <l.larouchalot@gmail.com>
2026-03-12 22:17:00 +0100

    Add Jaeger Service for better tracability

---
+++ posts/tracing.js
@@ -0,0 +1,21 @@
+const opentelemetry = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+
+
+const exporter = new JaegerExporter({
+  endpoint: 'http://jaeger-srv:14268/api/traces',
+});
+
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: exporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+  serviceName: 'posts-srv',
+});
+
+sdk.start();
+
+// Gérer l'arrêt propre
+process.on('SIGTERM', () => {
+  sdk.shutdown().then(() => console.log('Tracing terminated')).catch((error) => console.log('Error terminating tracing', error)).finally(() => process.exit(0));
+});
