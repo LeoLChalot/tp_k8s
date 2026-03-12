215573892e42a386718868258acd1729ce5b138d
Eloic Lesellier <e.lesellier@tech-nature.com>
2026-03-12 16:15:10 +0100

    database for query

--- query/index.js
+++ query/index.js
@@ -2,48 +2,77 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
+const mongoose = require("mongoose")
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = {};
+const postSchema = new mongoose.Schema({
+  id: String,
+  title: String,
+  comments: [
+    {
+      id: String,
+      content: String,
+      status: String,
+    },
+  ],
+});
 
-const handleEvent = (type, data) => {
+const Post = mongoose.model("Post", postSchema);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://query-mongo-srv:27017/query");
+    console.log("Connected to Query MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+connectDB();
+
+const handleEvent = async (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
 
-    posts[id] = { id, title, comments: [] };
-  }
+    const post = new Post({ id, title, comments: [] });
+    await post.save();  }
 
   if (type === "CommentCreated") {
     const { id, content, postId, status } = data;
 
-    const post = posts[postId];
-    post.comments.push({ id, content, status });
+    const post = await Post.findOne({ id: postId });
+    if (post) {
+      post.comments.push({ id, content, status });
+      await post.save();
+    }
   }
 
   if (type === "CommentUpdated") {
     const { id, content, postId, status } = data;
 
-    const post = posts[postId];
-    const comment = post.comments.find((comment) => {
-      return comment.id === id;
-    });
-
-    comment.status = status;
-    comment.content = content;
+    const post = await Post.findOne({ id: postId });
+    if (post) {
+      const comment = post.comments.find((c) => c.id === id);
+      if (comment) {
+        comment.status = status;
+        comment.content = content;
+        await post.save();
+      }
+    }
   }
 };
 
-app.get("/posts", (req, res) => {
+app.get("/posts", async (req, res) => {
+  const posts = await Post.find({});
   res.send(posts);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   const { type, data } = req.body;
 
-  handleEvent(type, data);
+  await handleEvent(type, data);
 
   res.send({});
 });
@@ -56,7 +85,7 @@ app.listen(4002, async () => {
     for (let event of res.data) {
       console.log("Processing event:", event.type);
 
-      handleEvent(event.type, event.data);
+      await handleEvent(event.type, event.data);
     }
   } catch (error) {
     console.log(error.message);
