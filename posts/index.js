11ea5b53e97d85c79c105cb60b5d10848509b2cc
Eloic Lesellier <e.lesellier@tech-nature.com>
2026-03-12 14:08:08 +0100

    kubernetes bdd on comments and posts

--- posts/index.js
+++ posts/index.js
@@ -1,37 +1,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { randomBytes } = require("crypto");
 const cors = require("cors");
 const axios = require("axios");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = {};
+const postSchema = new mongoose.Schema({
+  title: String,
+});
+const Post = mongoose.model("Post", postSchema);
 
-app.get("/posts", (req, res) => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://posts-mongo-srv:27017/posts");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+connectDB();
+
+app.get("/posts", async (req, res) => {
+  const posts = await Post.find({});
   res.send(posts);
 });
 
 app.post('/posts/create', async (req, res) => {
-  const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
-  posts[id] = {
-    id,
+  const post = new Post({
     title,
-  };
+  })
+  await post.save();
 
   await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
-      id,
+      id: post._id,
       title,
     },
   });
 
-  res.status(201).send(posts[id]);
+  res.status(201).send(post);
 });
 
 app.post("/events", (req, res) => {
